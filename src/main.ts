1d64fcff3e4b685144d182556f1012e87fe04b78
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2026-05-08 15:44:10 -0300

    feat(app): adiciona ValidationPipe para validação global de DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /** Remove propriedades que não estão no DTO */
+      whitelist: true,
+
+      /** Força erro quando propriedades não estão no DTO */
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
