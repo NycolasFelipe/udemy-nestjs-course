b79cc8cf860aa582be9f8b72d303796dc8449032
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2026-05-08 15:44:22 -0300

    feat(recados): adiciona validações e campos opcionais nos DTOs de criação e atualização de recados

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString({ message: 'Texto deve ser uma string.' })
+  @IsNotEmpty({ message: 'Texto é obrigatório.' })
+  @MinLength(5, { message: 'Texto deve ter pelo menos 5 caracteres.' })
+  @MaxLength(200, { message: 'Texto não pode ter mais de 200 caracteres.' })
   readonly texto: string;
+
+  @IsString({ message: 'De deve ser uma string.' })
+  @IsNotEmpty({ message: 'De é obrigatório.' })
   readonly de: string;
+
+  @IsString({ message: 'Para deve ser uma string.' })
+  @IsNotEmpty({ message: 'Para é obrigatório.' })
   readonly para: string;
 }
--- src/recados/dto/update-recado.dto.ts
+++ src/recados/dto/update-recado.dto.ts
@@ -1,5 +1,11 @@
-export class UpdateRecadoDto {
-  readonly texto?: string;
-  readonly de?: string;
-  readonly para?: string;
+import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+// DTOs
+import { CreateRecadoDto } from './create-recado.dto';
+
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
+  @IsBoolean({ message: 'O campo lido deve ser um valor booleano' })
+  @IsOptional()
+  readonly lido?: boolean;
 }
